564155fc2992dd0950de0269328b14e8a6eb3ed5
roberthamdalah <roberthamdalah@gmail.com>
2025-08-03 19:32:13 +0700

    #SEDINI Edit Login Form

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 "use strict";
-// function validateForm() {
-//   const nama = document.getElementById("nama").value.trim();
-//   const gender = document.getElementById("gender").value.trim().toLowerCase();
-//   const date = document.getElementById("date").value.trim();
-
-//   if (nama.length < 3) {
-//     alert("Nama lengkap wajib diisi");
-//     return false;
-//   }
-
-//   if (gender !== "l" && gender !== "p") {
-//     alert("Jenis kelamin wajib diisi");
-//     return false;
-//   }
-
-//   if (date.length < 5) {
-//     alert("Tanggal Lahir tidak boleh kosong.");
-//     return false;
-//   }
-
-//   return true; // lanjut submit
-// }
-
 const form = document.getElementById("loginForm");
 
 form.addEventListener("submit", function (e) {
